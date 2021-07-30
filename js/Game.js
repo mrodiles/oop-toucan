8d3f8f59cea5ad24b41a15d69cc38d6350814097
“Michael <michaelrodiles@gmail.com>
2021-07-29 21:54:44 -0400

    oop toucan 3

--- js/Game.js
+++ js/Game.js
@@ -3,22 +3,32 @@ class Game {
         this.canvas = document.getElementById('canvas');
         this.context = this.canvas.getContext('2d');
         this.score = 1;
-        this.timer = 0;
+        this.timer = 60;
         this.backgroundImg = new Component(this, 0, 0, 1000, 900, './images/background.jpg')
         this.cloudImg = new Component(this, 0, -60, 1050, 400, "./images/clouds.png")
         this.toucanImg = new Component(this, 755, 500, 250, 300, './images/toucan2.png')
         this.leafImg = new Component(this, 600, 5, 300, 200, './images/scoreLeaf.png')
         this.pineappleImg = new Component(this, 500, 50, 50, 75, './images/pineapple.png', 2)
-        this.bananaImg = new Component(this, 500, 50, 50, 75,'./images/banana.png', 1)
-        this.papayaImg = new Component(this, 500, 50, 50, 75,'./images/papaya.png', 4)
-        this.watermelonImg = new Component(this, 500, 50, 50, 75,'./images/watermelon.png', 5)
-        this.pomImg = new Component(this, 500, 50, 50, 75,'./images/pom.png', 3)
-        this.appleImg = new Component(this, 500, 50, 50, 75,'./images/apple.png', 3.5)
-        
+        this.bananaImg = new Component(this, 500, 50, 50, 75, './images/banana.png', 1)
+        this.papayaImg = new Component(this, 500, 50, 50, 75, './images/papaya.png', 4)
+        this.watermelonImg = new Component(this, 500, 50, 50, 75, './images/watermelon.png', 5)
+        this.pomImg = new Component(this, 500, 50, 50, 75, './images/pom.png', 3)
+        this.appleImg = new Component(this, 500, 50, 50, 75, './images/apple.png', 3.5)
+
         this.fruits = [this.pineappleImg, this.bananaImg, this.papayaImg, this.watermelonImg, this.pomImg, this.appleImg]
+
+        this.startButton = new Component(this, 500,250,400,200, './images/startButton.png')
+
+        this.gameoverToucanImg = new Component(this, 755, 500, 250, 300, 300, './images/goToucan.png')
+        
+        this.gameTitleImg = new Component(this, 500, 0, 500, 300, 300, './images/You-Can-Toucan.png')
+        
+        this.gameoverImg = new Component(this, 300, 250, 350, 350, './images/gameover.png')
         
 
-        this.gameoverImg = new Component(this, 300, 250, 350, 350, './images/gameover.png')
+
+
+
 
     }
 
@@ -34,7 +44,7 @@ class Game {
         })
         // Start game timer
         this.startTimer();
-  
+
 
     }
 
@@ -66,13 +76,13 @@ class Game {
 
         this.context.fillText(`Score: ${this.score}`, 700, 140);
 
-       
+
     }
 
     drawLoop(fruit) {
         // Clear image every iteration
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        
+
         // Setting Fruit falling speed on y axis
         fruit.y += fruit.speed;
         // Reset coordinates after reaching end of window height (y axis)
@@ -80,7 +90,7 @@ class Game {
             fruit.y = 0;
             fruit.x = Math.random() * (canvas.width - 250);
         }
-        
+
         // Check if timer is up, if not loop animation via recursion
         requestAnimationFrame(() => {
             if (this.timer >= 120) {
--- js/index.js
+++ js/index.js
@@ -1,9 +1,15 @@
-// toucan
-//fruit
-// game
+const playButton = document.querySelector('.button');
+
+const mainPage = document.getElementById('main-page');
+
+const canvasHtml = document.getElementById('canvas');
+
+playButton.addEventListener('click', () => {
+    mainPage.classList.toggle('hidden');
+    canvasHtml.style.display = 'block';
+    game.start();
+})
+
 
 
 const game = new Game();
-
-game.start();
-
